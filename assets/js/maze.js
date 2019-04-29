40d68e19f0a5eee5b33026d96432d443eadc09b9
Marco Biedermann <marco.biedermann@moovel.com>
2019-04-29 22:22:27 +0200

    feat: canvas
    setup canvas and context

--- assets/js/maze.js
+++ assets/js/maze.js
@@ -1,13 +1,26 @@
 class Maze {
   constructor(element, options) {
     const defaults = {
-
+      width: 400,
+      height: 400,
     };
 
+    this.element = element;
     this.options = {
       ...defaults,
       ...options,
     };
+
+    this.init();
+  }
+
+  init() {
+    const { element, options } = this;
+
+    this.context = element.getContext('2d');
+
+    this.element.width = options.width;
+    this.element.height = options.height;
   }
 }
 
