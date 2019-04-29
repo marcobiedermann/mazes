2790d487bf0f1f5a954b27d91a71ac759112573f
Marco Biedermann <marco.biedermann@moovel.com>
2019-04-29 22:31:43 +0200

    feat: render
    add render method

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
 import Maze from './maze.js';
 
 const maze = new Maze(document.querySelector('.js-maze'));
+
+maze.render();
--- assets/js/maze.js
+++ assets/js/maze.js
@@ -1,8 +1,9 @@
 class Maze {
   constructor(element, options) {
     const defaults = {
-      width: 400,
-      height: 400,
+      width: 300,
+      height: 300,
+      tileSize: 20,
     };
 
     this.element = element;
@@ -10,6 +11,24 @@ class Maze {
       ...defaults,
       ...options,
     };
+
+    this.cells = [
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+      [0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0],
+      [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0],
+      [0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+      [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+      [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0],
+      [0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0],
+      [0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0],
+      [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+      [0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 0],
+      [0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+      [0, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0],
+      [0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0],
+      [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0],
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    ]
 
     this.init();
   }
@@ -21,6 +40,23 @@ class Maze {
 
     this.element.width = options.width;
     this.element.height = options.height;
+  }
+
+  render() {
+    const { context, cells, options } = this;
+
+    const rows = cells.length;
+
+    for(let i = 0; i < rows; i += 1) {
+      const cols = cells[i].length;
+
+      for(let j = 0; j < cols; j += 1) {
+        const cell = cells[i][j];
+
+        context.fillStyle = cell ? '#fff' : '#000';
+        context.fillRect(j * options.tileSize, i * options.tileSize, options.tileSize, options.tileSize);
+      }
+    }
   }
 }
 
